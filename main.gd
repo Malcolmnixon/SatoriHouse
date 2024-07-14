20050a32f21b3a146f19684155577c98c990a948
Malcolm Nixon <Malcolm.nixon@gmail.com>
2024-07-13 21:11:15 -0400

    Added support for saving the decorations if the anchor manager extension is available.

--- main.gd
+++ main.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 
+const SAVE_FILE := "user://room.json"
 const SOUND_RENDER_LAYER := 0x80000
 const SOUND_PHYSICS_LAYER := 0x80000000
 
@@ -15,6 +16,7 @@ func _ready() -> void:
 	# Test if the anchor manager is real
 	if %AnchorManager.is_class("OpenXRFbSpatialAnchorManager"):
 		Game.can_save = true
+		_load()
 
 
 func _on_show_audio(p_visible : bool) -> void:
@@ -41,9 +43,87 @@ func _on_create_decoration(p_decoration : DecorationType) -> void:
 	instance.global_position = pos
 
 
+func _load() -> void:
+	# Open the file
+	var file := FileAccess.open(SAVE_FILE, FileAccess.READ)
+	if not file:
+		return
+
+	# Parse the file
+	var json := JSON.new()
+	if json.parse(file.get_as_text()) != OK:
+		return
+
+	# Verify we have a dictionary
+	if json.data is not Dictionary:
+		return
+
+	# Process the data
+	var anchor_data : Dictionary = json.data
+	if anchor_data.size() == 0:
+		return
+
+	# Load the anchors
+	%AnchorManager.load_anchors(anchor_data.keys(), anchor_data)
+
+	# Wait for all decorations anchors to be created
+	while %AnchorManager.get_anchor_uuids().size() < anchor_data.size():
+		await get_tree().create_timer(0.1).timeout
+
+	# Construct the decorations
+	for uuid in anchor_data:
+		var entity = %AnchorManager.get_spatial_entity(uuid)
+		if not entity:
+			continue
+
+		var anchor : XRAnchor3D = %AnchorManager.get_anchor_node(uuid)
+		if not anchor:
+			continue
+
+		var custom_data : Dictionary = entity.custom_data
+		var instance_scene : String = custom_data["scene"]
+
+		# Create the instance
+		var packed_scene : PackedScene = load(instance_scene)
+		var instance : Node3D = packed_scene.instantiate()
+
+		# Add to the scene
+		%Decorations.add_child(instance)
+		instance.global_position = anchor.global_position
+
+
 func _save() -> void:
-	# TODO: Add Spatial Anchors Support
-	pass
+	# Clear all UUIDs
+	for uuid in %AnchorManager.get_anchor_uuids():
+		%AnchorManager.untrack_anchor(uuid)
+
+	# Create anchors for all decorations
+	var decorations := 0
+	for node in %Decorations.get_children():
+		var decoration := node as Decoration
+		if not decoration:
+			continue
+
+		# Create an anchor for the decoration
+		var custom_data := { "scene" : decoration.type.instance_scene }
+		%AnchorManager.create_anchor(decoration.global_transform, custom_data)
+		decorations += 1
+
+	# Wait for all decorations anchors to be created
+	while %AnchorManager.get_anchor_uuids().size() < decorations:
+		await get_tree().create_timer(0.1).timeout
+
+	# Populate the anchor data to save
+	var anchor_data := {}
+	for uuid in %AnchorManager.get_anchor_uuids():
+		var entity = %AnchorManager.get_spatial_entity(uuid)
+		anchor_data[uuid] = entity.custom_data
+
+	# Save the anchor file
+	var file := FileAccess.open(SAVE_FILE, FileAccess.WRITE)
+	if file:
+		file.store_string(JSON.stringify(anchor_data))
+		file.close()
 
 
 func _quit() -> void:
