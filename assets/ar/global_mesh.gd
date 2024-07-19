079fc8f9ef9e98884c25d0e60a18fdbc7e96427b
Malcolm Nixon <Malcolm.nixon@gmail.com>
2024-07-19 09:57:39 -0400

    Minor post-jam fixes for poses, global-mesh occlusion, and grab-by-pinch.

--- assets/ar/global_mesh.gd
+++ assets/ar/global_mesh.gd
@@ -1,9 +1,18 @@
 extends StaticBody3D
 
 
+@export var material : Material
+
+
 func setup_scene(entity : OpenXRFbSpatialEntity) -> void:
 	# Construct the collison shape
 	var collision_shape := entity.create_collision_shape()
 	if collision_shape:
 		add_child(collision_shape)
 		Game.has_global_mesh = true
+
+	# Construct the collision mesh
+	var mesh_instance := entity.create_mesh_instance()
+	if mesh_instance:
+		add_child(mesh_instance)
+		mesh_instance.material_override = material
